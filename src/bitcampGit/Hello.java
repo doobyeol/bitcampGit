69e23635530ad6e3fb42109ef617d3ba1813ac89
yh Kim <yuhwan@kakao.com>
2021-01-06 19:36:47 +0900

    로컬 두번째

--- src/bitcampGit/Hello.java
+++ src/bitcampGit/Hello.java
@@ -7,16 +7,28 @@ public class Hello {
 		System.out.println("작동연습중");
 		System.out.println("안녕하세요!!");
 		System.out.println("안녕하세요!!");
-		System.out.println("안녕                                                            하세요!!");
+
+		
+		System.out.println("안녕하                                                      세요!!");
+		
+		
+		
 		System.out.println("안녕하세요!!");
+		
+		
+		System.out.println("안녕하                                                      세요!!");
+		
+		
+		
 		System.out.println("안녕하세요!!");
+		
+		
+		System.out.println("안녕하                                                      세요!!");
+		
+		
+		
 		System.out.println("안녕하세요!!");
-		System.out.println("안녕																	하세요!!");
-		System.out.println("안녕하세요!!");
-		System.out.println("안녕하세요!!");
-		System.out.println("안녕하세요!!");
-		System.out.println("잘만들었다!!");
-		System.out.println("안녕하세요!!");
+		
 		
 		System.out.println("안녕하                                                      세요!!");
 		
