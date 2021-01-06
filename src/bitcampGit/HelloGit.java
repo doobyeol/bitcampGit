fcfed0dbc3d01df8d2ddb98f0dadc0e7160b6ee5
lssgt <lssgt@DESKTOP-C4PDKK0>
2021-01-06 19:07:41 +0900

    tesijfoikjesf

--- src/bitcampGit/HelloGit.java
+++ src/bitcampGit/HelloGit.java
@@ -3,7 +3,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("HelloGitWorld!");
-		System.out.println("제가 한번 수정해보겠습니다.a");
+		System.out.println("제가 한번 수정해보겠습니다df.a");
 		new Hello().say();
 
 	}
